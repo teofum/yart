da001be7a2fb110c71e7029c6142142f244cc2fd
Teo Fumagalli <teo.fum@gmail.com>
2024-09-14 17:23:04 -0300

    update sdl2 version

--- src/main.cpp
+++ src/main.cpp
@@ -25,22 +25,23 @@ int main() {
   yart::Buffer buffer(900, 600); // 3:2 medium
 //  yart::Buffer buffer(1920, 1200); // 16:10 large
 //  yart::Buffer buffer(400, 400); // 1:1 small
+//  yart::Buffer buffer(800, 800); // 1:1 medium
 //  yart::Buffer buffer(1280, 1280); // 1:1 large
 
-  yart::Camera camera({buffer.width(), buffer.height()}, 24.0f, 8.0f);
+  yart::Camera camera({buffer.width(), buffer.height()}, 50.0f, 1.8f);
 //  camera.apertureSides = 7;
-  camera.exposure = 1.0f;
+//  camera.exposure = -1.0f;
 
 //  camera.moveAndLookAt({0.0f, 5.0f, 15.0f}, {0.0f, 5.0f, 0.0f}); // Cornell Box
 //  camera.moveAndLookAt({0.0f, 5.0f, 15.0f}, {0.0f, 1.0f, 0.0f}); // Mat test
 //  camera.moveAndLookAt({5.28f, 0.96f, 0.0f}, {2.57f, 1.09f, 1.1f}); // Sponza
 //  camera.moveAndLookAt({8.5f, 1.8f, 0}, {0, 3.2f, 0}); // New Sponza
-  camera.moveAndLookAt({-32.2f, 3.5f, -14.1f}, {-8.2f, 6.1f, -0.95f}); // Bistro
+//  camera.moveAndLookAt({-32.2f, 3.5f, -14.1f}, {-8.2f, 6.1f, -0.95f}); // Bistro
 //  camera.moveAndLookAt({-32.2f, 3.5f, -14.1f}, {-7.1f, 4.0f, -0.95f}); // Bistro
 //  camera.moveAndLookAt({-11.2f, 2.0f, -1.7f}, {-8.1f, 2.2f, 0.33f}); // Bistro
 //  camera.moveAndLookAt({6.1f, 0.4f, 0.46f}, {1.5f, 0.5f, 0.3f}); // Car
 //  camera.moveAndLookAt({6.1f, 0.4f, 0.46f}, {1.5f, 0.5f, -0.25f}); // Cars
-//  camera.moveAndLookAt({6.3f, 0.83f, 0.46f}, {4.0f, 0.66f, 0.1f}); // Porsche
+  camera.moveAndLookAt({6.3f, 0.83f, 0.46f}, {4.0f, 0.66f, 0.1f}); // Porsche
 //  camera.moveAndLookAt({-10.3f, 1.0f, 9.9f}, {0.14f, 0.67f, 0.96f}); // Porsche
 //  camera.moveAndLookAt({2.16f, 0.68f, -0.72f}, {1.7f, 0.82f, 0.17f}); // Porsche
 //  camera.moveAndLookAt({-1.9f, 0.68f, 0.37f}, {-0.11f, 0.64f, 0.3f}); // Porsche
@@ -49,7 +50,7 @@ int main() {
 //  camera.moveAndLookAt({5.0f, 50.0f, 5.0f}, {}); // City
 //  camera.moveAndLookAt({11.07f, -0.98f, 10.62f}, {0.0f, 0.28f, 1.55f});
 
-  std::unique_ptr<yart::Scene> scene = load("models/bistro.glb");
+  std::unique_ptr<yart::Scene> scene = load("models/porsche.glb");
 
   yart::HDRTexture hdri = yart::loadTextureHDR("hdris/rosendal_plains_2_oct.hdr");
   auto envLight = yart::ImageInfiniteLight(100.0f, &hdri);
@@ -64,7 +65,7 @@ int main() {
     camera
   );
   renderer.scene = scene.get();
-  renderer.samples = 256;
+  renderer.samples = 64;
   renderer.tonemapper = &tonemapper;
 
   yart::frontend::MetalSDLFrontend frontend(&renderer);
